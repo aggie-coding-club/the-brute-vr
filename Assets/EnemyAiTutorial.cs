62b0b4cd1c5f79c302099a5b891a3a9e40bb6dcf
yuki <yuki@tamu.edu>
2022-11-14 19:41:36 -0600

    Finished Enemy AI

--- Assets/EnemyAiTutorial.cs
+++ Assets/EnemyAiTutorial.cs
@@ -28,7 +28,7 @@ public class EnemyAiTutorial : MonoBehaviour
 
     private void Awake()
     {
-        player = GameObject.Find("PlayerObj").transform;
+        player = GameObject.Find("Player").transform;
         agent = GetComponent<NavMeshAgent>();
     }
 
