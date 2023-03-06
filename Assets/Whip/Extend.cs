7701d077549e7a6bccb9b6ee321bc36afc7116d5
Shlok-Bhakta <shlokbhakta1@gmail.com>
2023-03-06 10:35:07 -0600

    giving up st script

--- Assets/Whip/Extend.cs
+++ Assets/Whip/Extend.cs
@@ -1,9 +1,12 @@
+using System.Data;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Extend : MonoBehaviour
 {
+    public Transform GameObject;
+    // I have given up on coding this garbage ima use the visual scripting instead
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,47 @@ public class Extend : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        print("HELLO");
+        Vector3 vector = getRotation();
+        print(vector);
     }
+
+    public Vector3 getRotation(){
+        float x_rotation = 0;
+        float y_rotation = 0;
+        float z_rotation = 0;
+
+        if(this.eulerAngles.x <= 180f)
+        {
+            x_rotation = this.eulerAngles.x;
+        }
+        else
+        {
+            x_rotation = this.eulerAngles.x - 360f;
+        }
+
+        if(this.eulerAngles.y <= 180f)
+        {
+            y_rotation = this.eulerAngles.y;
+        }
+        else
+        {
+            y_rotation = this.eulerAngles.y - 360f;
+        }
+
+        if(this.eulerAngles.z <= 180f)
+        {
+            y_rotation = this.eulerAngles.z;
+        }
+        else
+        {
+            y_rotation = this.eulerAngles.z - 360f;
+        }
+
+        Vector3 output = transform.rotation;
+        output.x = x_rotation;
+        output.y = y_rotation;
+        output.z = z_rotation;
+        return output;
+    }
+
 }
