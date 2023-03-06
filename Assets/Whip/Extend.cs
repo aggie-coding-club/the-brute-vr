ad3dd13e254e00c1435cb445dc1f4291d6bba054
Shlok-Bhakta <shlokbhakta1@gmail.com>
2023-03-06 10:17:24 -0600

    simple hello world Test

---
+++ Assets/Whip/Extend.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Extend : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        print("HELLO");
+    }
+}
