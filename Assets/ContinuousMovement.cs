dfeb7bed4c6c7743fce0766859f853becd46896e
allanaziz581 <allan.a1@icloud.com>
2023-04-12 01:00:50 -0500

    demo

---
+++ Assets/ContinuousMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+using Unity.XR.CoreUtils;
+
+
+public class ContinuousMovement : MonoBehaviour
+{
+    public XRNode inputSource;
+    public float speed = 1;
+
+    private XROrigin rig;
+    private Vector2 inputAxis;
+    private CharacterController character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+        rig = GetComponent<XROrigin>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+        device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis);
+    }
+
+    private void FixedUpdate()
+    {
+        Quaternion headYaw = Quaternion.Euler(0, rig.Camera.transform.eulerAngles.y, 0);
+        Vector3 direction = headYaw *  new Vector3(inputAxis.x, 0, inputAxis.y);
+
+        character.Move(direction * Time.fixedDeltaTime * speed);
+    }
+}
+
+
